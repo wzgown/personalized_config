ffc28bb474f9fb01b8158dbc266b7db8879996ef
lance.wang <lance.wang@ucloud.cn>
2022-01-14 14:36:51 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 direnv hook fish | source
 
-set -x GOROOT /usr/local/opt/go/libexec
-set -x GOPATH /Users/wangzhigang/workspace/golang/3rdpkg /Users/wangzhigang/workspace/golang/xishiqu.com /Users/wangzhigang/workspace/golang/mihoyo
-set -x GOBIN /Users/wangzhigang/workspace/golang/3rdpkg/bin
-set -x PATH $PATH $GOROOT/bin $GOBIN
-set -x no_proxy gopkg.mihoyo.com,platgit.mihoyo.com
-set -x http_proxy http://0.0.0.0:1087
-set -x https_proxy http://0.0.0.0:1087
-
-abbr -a ll ls -l
 abbr -a ni nvim
 abbr -a vim nvim
 abbr -a gob go build
 abbr -a gst git status
 abbr -a gco git checkout
-abbr -a platgo cd /Users/wangzhigang/workspace/golang/mihoyo/src
-abbr -a takumi cd /Users/wangzhigang/workspace/golang/zhigang.wang/src/gopkg.mihoyo.com/takumi
 abbr -a code '/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
+alias ls=exa
+alias ll='exa --long --header --git'
+# 还原docker的启动命令
+alias runlike="docker run --rm -v /var/run/docker.sock:/var/run/docker.sock assaflavie/runlike"
+# 用bat来显示man文档; bat是更先进的cat
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+# ucloud
+alias ucd='cd /Users/user/workspace/ucloud'
+set -x PATH $PATH /Users/user/workspace/ucloud/bin /Users/user/go/bin
